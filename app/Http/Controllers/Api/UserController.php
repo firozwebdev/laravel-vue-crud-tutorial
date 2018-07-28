960a6cde6fe6ccfae2e9b493df07dec78bb8c000
sabuzwebdev <frsmgt@gmail.com>
2018-07-29 03:01:47 +0600

    Api UserController is creating

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all(); // it will retrieve all users.
+        return response()->json($users,200);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $user) // this is called implicit model binding
+    {
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->image = '1.jpg'; // we will work for it later.
+        $user->password = bcrypt( $request->get('password'));
+
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
